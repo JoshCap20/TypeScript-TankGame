98ae0508567957260feb7d806912988afc0aad5a
Josh Caponigro <jcaponigro20@gmail.com>
2023-08-12 19:09:38 -0500

    Add WSAD key support

--- public/player.js
+++ public/player.js
@@ -9,18 +9,22 @@ export function handleMovement(ws, tankInfo) {
     window.addEventListener('keydown', (event) => {
         const radianRotation = (position.rotation * Math.PI) / 180;
         switch (event.key) {
+            case "KeyW":
             case 'ArrowUp':
                 position.x -= Math.sin(radianRotation) * tank.speed;
                 position.y += Math.cos(radianRotation) * tank.speed;
                 break;
+            case "KeyS":
             case 'ArrowDown':
                 position.x += Math.sin(radianRotation) * tank.speed;
                 position.y -= Math.cos(radianRotation) * tank.speed;
                 break;
+            case "KeyA":
             case 'ArrowLeft':
                 position.rotation -= 5;
                 if (position.rotation < 0) position.rotation += 360; // Keep rotation within 0-360
                 break;
+            case "KeyD":
             case 'ArrowRight':
                 position.rotation += 5;
                 if (position.rotation >= 360) position.rotation -= 360; // Keep rotation within 0-360
